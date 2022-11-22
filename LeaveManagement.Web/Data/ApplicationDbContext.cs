f2141927da6bc600ff7a3f98477430b6a160c7ad
Colt Skeen <colt.skeen@gesa.com>
2022-11-22 13:41:56 -0800

    Added Database Tables and Migrations, Database Setup

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace LeaveManagement.Web.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
+namespace LeaveManagement.Web.Data;
+public class ApplicationDbContext : IdentityDbContext<Employee>
+{ 
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
 }
